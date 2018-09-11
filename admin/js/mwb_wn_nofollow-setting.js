64950bc9fbca144c4e163200dc33b4227e32ac18
eshita-cedcoss <eshita@cedcoss.com>
2018-09-11 13:48:05 +0530

    qa issues solved

--- admin/js/mwb_wn_nofollow-setting.js
+++ admin/js/mwb_wn_nofollow-setting.js
@@ -1,53 +1,42 @@
 jQuery(document).ready(function(jQuery){
   setTimeout(function(){jQuery(".notice-success").fadeOut()},2000);
   jQuery("#mwb_remove_rel_link").on("change",function(){
-  	    var ajax_url=mwb_wt.ajax_url;
-		var security=mwb_wt.security;
-		var rel=mwb_wt.rel;
-		var rel_internal=mwb_wt.rel_internal;
-		jQuery.ajax({
-			method: "POST",
-			url: ajax_url,
-			data: { action: "mwb_wn_request",
-			security: security },
-			success: function(response)
-			{
-				jQuery("#mwb_add_rel_link").attr('checked', false); 
-				jQuery("#mwb_add_internal_rel_link").attr('checked', false); 
-	        }
-         });
+  
+		jQuery("#mwb_add_rel_link").attr('checked', false); 
+		jQuery("#mwb_add_internal_rel_link").attr('checked', false); 
+	  
      });
    jQuery("#mwb_add_rel_link").on("change",function(){
-  	    var ajax_url=mwb_wt.ajax_url;
-		var security=mwb_wt.security;
-		var rel=mwb_wt.rel;
-		var rel_internal=mwb_wt.rel_internal;
-		jQuery.ajax({
-			method: "POST",
-			url: ajax_url,
-			data: { action: "mwb_wn_request",
-			security: security },
-			success: function(response)
-			{
-				jQuery("#mwb_remove_rel_link").attr('checked', false); 
-	        }
-         });
-     });
+   
+    	jQuery("#mwb_remove_rel_link").attr('checked', false); 
+	   	var checked=jQuery("#mwb_add_internal_rel_link").attr('checked');
+	   	
+	  	if(jQuery("#mwb_add_internal_rel_link").attr('checked'))
+	  	{
+	  		jQuery("#mwb_add_rel_link").attr('checked', false);
+	  		alert("You have already checked add rel=nofollow to all links . You have to disable the Add rel=nofollow to all links checkbox");
+	  	}
+	  	else
+	  	{
+	  		jQuery("#mwb_add_rel_link").attr('checked', true);
+	  	}
+    });
     jQuery("#mwb_add_internal_rel_link").on("change",function(){
-  	    var ajax_url=mwb_wt.ajax_url;
-		var security=mwb_wt.security;
-		var rel=mwb_wt.rel;
-		var rel_internal=mwb_wt.rel_internal;
-		jQuery.ajax({
-			method: "POST",
-			url: ajax_url,
-			data: { action: "mwb_wn_request",
-			security: security },
-			success: function(response)
-			{
-				jQuery("#mwb_remove_rel_link").attr('checked', false); 
-				jQuery("#mwb_add_rel_link").attr('checked', false); 
-	        }
-         });
+    	
+    	
+  	    jQuery("#mwb_remove_rel_link").attr('checked', false); 
+		jQuery("#mwb_add_rel_link").attr('checked', false); 
+  	
      });
+    jQuery("#mwb_add_alt_image").on("change",function(){
+       
+       if(jQuery("#mwb_add_alt_image").attr('checked'))
+	  	{
+	  		alert("Adding alt attribute to the image tag");
+	  	}
+	  	else
+	  	{
+	  		alert("Removing alt attribute from the image tag");
+	  	}
+    })
 });
